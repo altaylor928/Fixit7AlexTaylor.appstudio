11369a8a03c6356b4dd2d680fdb82aee0f6613e5
altaylor928 <60445248+altaylor928@users.noreply.github.com>
2020-02-09 22:12:56 -0600

    Create Stringmethods.js

---
+++ forms/Stringmethods/Stringmethods.js
@@ -0,0 +1,6 @@
+//create variable
+let quoteString = "Failure is simply the opportunity to begin again this time more intelligently."
+
+//make string uppercase
+let upperCaseString = quoteString.toUpperCase()
+console.log(upperCaseString)
